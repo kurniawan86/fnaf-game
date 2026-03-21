4ca705f323dce2ff4083cc6c91a7d4a1391d23c1
muchamadkurniawan <muchamad.kurniawan@itats.ac.id>
2026-03-21 19:39:07 +0700

    newgame

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+// Script sederhana untuk mengetes hosting PHP
+
+echo "<h1>Testing Hosting Berhasil!</h1>";
+echo "<p>Jika Anda melihat pesan ini, berarti file PHP dapat dijalankan dengan baik di hosting Anda.</p>";
+
+echo "<hr>";
+
+// Menampilkan informasi detail tentang konfigurasi PHP di server
+// Sangat berguna untuk memastikan versi PHP dan modul-modul yang aktif
+phpinfo();
+?>
